c1b72adee10be7199afa7c7d9a3fa1a87ed19b7b
Ashwani Rathee <ab669522@gmail.com>
2021-01-01 19:03:33 +0530

    Remove cd and structures

--- structures/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class StructuresConfig(AppConfig):
-    name = 'structures'
--- structures/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from django.conf.urls import url
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns = [
-    path('', views.index, name='index'),
-]
