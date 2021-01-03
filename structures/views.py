31669f28070f9a5e9dc11a69d6d377cd5cb05112
gurnameh-99 <gurunameh.work@gmail.com>
2021-01-03 16:00:54 +0530

    automated server deletion of images

--- structures/views.py
+++ structures/views.py
@@ -7,7 +7,6 @@ from django.shortcuts import render, redirect
 from .forms import *
 from rdkit import Chem
 import os
-import pubchempy as pcp
 
 # Create your views here.
 def index(request):
@@ -15,15 +14,23 @@ def index(request):
         myfile = request.FILES['myfile']
         fs = FileSystemStorage()
         filename = fs.save(myfile.name, myfile)
+        cmd = "java -jar ./structures/molvec-0.9.8-jar-with-dependencies.jar molvec -f ./structures/media/" + filename +" -o ./structures/static/data.mol"
+        result = os.system(cmd)
+        if result == 0:
+            cmd = "rm ./structures/media/" + filename
+            os.system(cmd)
+        m = Chem.MolFromMolFile('structures/static/data.mol')
+        smile = Chem.MolToSmiles(m)
         return redirect('viewer')
     return render(request, 'structures/index.html')
 
 def data(request):
-    cmd = "java -jar ./structures/molvec-0.9.8-jar-with-dependencies.jar molvec -f ./structures/input/aldehyde.jpeg -o ./structures/result/aldehyde.mol"
+
+    cmd = "java -jar ./structures/molvec-0.9.8-jar-with-dependencies.jar molvec -f ./structures/media/" + filename +" -o ./structures/static/data.mol"
     returned_value = os.system(cmd)
-    m = Chem.MolFromMolFile('structures/result/aldehyde.mol')
+    m = Chem.MolFromMolFile('structures/static/data.mol')
     smile = Chem.MolToSmiles(m)
-    return HttpResponse(smile)
+    return redirect('viewer')
 def getmol(request):
     cmd = "java -jar ./structures/molvec-0.9.8-jar-with-dependencies.jar molvec -f ./structures/input/aldehyde.jpeg -o ./structures/result/aldehyde.mol"
     returned_value = os.system(cmd)
@@ -69,9 +76,4 @@ def periodic_table(request):
     return render(request,'structures/periodic_table.html')
 
 def viewer(request):
-    m = Chem.MolFromMolFile('structures/result/aldehyde.mol')
-    smile = Chem.MolToSmiles(m)
-    c = pcp.Compound.from_cid(5000)
-    print(c.molecular_formula,"   ",c.molecular_weight)
-    
     return render(request,'structures/viewer.html')
